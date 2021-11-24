59b2c804104859f9bc3a0ba10f17be196bf895b8
lisongyu <269876940@qq.com>
2021-11-25 06:39:25 +0800

    feat(uo): ui基础添加

--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,17 +1,26 @@
 <template>
-  <div class="user">
-    <h2>user</h2>
-  </div>
+	<div class="user">
+		<hy-form v-bind="searchFormConfig" />
+		<div class="content"></div>
+	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue"
+import HyForm from "@/base-ui/form"
+
+import { searchFormConfig } from "./config/search.config"
 
 export default defineComponent({
-  name: 'user',
-  setup() {
-    return {}
-  }
+	name: "user",
+	components: {
+		HyForm
+	},
+	setup() {
+		return {
+			searchFormConfig
+		}
+	}
 })
 </script>
 
