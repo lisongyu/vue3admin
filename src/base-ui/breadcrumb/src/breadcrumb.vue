30cb7e3a32509a471d0b4b8f2d8d6a771581a9e1
lisongyu <269876940@qq.com>
2021-12-06 22:27:12 +0800

    feat: 添加ui组建

--- src/base-ui/breadcrumb/src/breadcrumb.vue
+++ src/base-ui/breadcrumb/src/breadcrumb.vue
@@ -1,29 +1,29 @@
 <template>
-  <div class="nav-breadcrumb">
-    <el-breadcrumb separator="/">
-      <template v-for="item in breadcrumbs" :key="item.name">
-        <el-breadcrumb-item :to="{ path: item.path }">{{
-          item.name
-        }}</el-breadcrumb-item>
-      </template>
-    </el-breadcrumb>
-  </div>
+	<div class="nav-breadcrumb">
+		<el-breadcrumb separator="/">
+			<template v-for="item in breadcrumbs" :key="item.name">
+				<el-breadcrumb-item :to="{ path: item.path }">{{
+					item.name
+				}}</el-breadcrumb-item>
+			</template>
+		</el-breadcrumb>
+	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
-import { IBreadcrumb } from '../types'
+import { defineComponent, PropType } from "vue"
+import { IBreadcrumb } from "../types"
 
 export default defineComponent({
-  props: {
-    breadcrumbs: {
-      type: Array as PropType<IBreadcrumb[]>,
-      default: () => []
-    }
-  },
-  setup() {
-    return {}
-  }
+	props: {
+		breadcrumbs: {
+			type: Array as PropType<IBreadcrumb[]>,
+			default: () => []
+		}
+	},
+	setup() {
+		return {}
+	}
 })
 </script>
 
